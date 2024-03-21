1415c694b17807b55d75e26a1af3175598860c53
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:23:50 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+// Equality and  Inequality Test
+console.log("Equality test with strings", "Apple" === "Apple");
+// Equality and Inequality Test
+console.log("Inequality test with strings", "Apple" != "orange");
+//Test  using the lower case function
+console.log("Lower Case function test:", "Hello".toLowerCase() === "hello");
+// Numerical tests involving equality 
+console.log("Equality test with number:", 20 === 20);
+//Numerical tests involving inequality
+console.log("Inequality test with number:", 20 != 34);
+// greater than and less than
+console.log("Greater than test:", 10 > 5);
+// greater than or equal to
+console.log("Greater than and equal to test:, 10 >= 10");
+//less than or equal to
+console.log("less than or equal to test:", 5 <= 10);
+//Test using "and" operator
+console.log("And operator test:", 5 === 5 && 10 > 5);
+//Test using "or" operator
+console.log("or operator test:", 5 === 5 || false);
+//Test whether an item is in array
+const fruits = ["Naspati", "Banana", "Amrood"];
+console.log('Test "Naspati"in the array:', fruits.includes("Naspati"));
+//Test whether an item is not in a array
+console.log('Testing "Apple" is not in array:', !fruits.includes('Apple'));
